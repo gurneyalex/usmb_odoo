ce9ee97fc3d24e9f58d6c0778ed2e22089abca3b
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2022-02-09 09:59:42 +0100

    create empty module

---
+++ local-modules/webapp/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2022 fill me in
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "USMB Webapp",
+    "summary": """webapp from USMP""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "USMB",
+    "depends": ["sale_import_rest"],
+    "data": [],
+}
