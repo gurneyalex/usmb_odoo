a7fdfcd39172bba62a710be4f5563e8f6a8f88c8
TheVatsu <vatsuna74@gmail.com>
2022-05-04 12:19:15 +0200

    Revert "fix erreur controller"
    This reverts commit cdcfbf37fffdd50c5cb0b220f1effe9f32f93d17.

--- local-modules/webapp/controllers/main.py
+++ local-modules/webapp/controllers/main.py
@@ -6,8 +6,8 @@ DIRECTORY = "../static/src/"
 class Academy(http.Controller):
 
     @http.route('/projet/', auth='public')
-    def index(self, *args, **kw):
-        super().__init__(*args, directory=DIRECTORY, **kw)
+    def index(self, **kw):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
 
 
 class WebappBaseController(RestController):
