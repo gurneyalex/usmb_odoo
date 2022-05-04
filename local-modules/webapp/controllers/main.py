ccf9fb08cd8537e78753ea7796f6dbb3df616b3a
TheVatsu <vatsuna74@gmail.com>
2022-05-04 12:19:09 +0200

    Revert "controller test"
    This reverts commit 88dac02e953c0296763c9a1b7a894d5f9e88a531.

--- local-modules/webapp/controllers/main.py
+++ local-modules/webapp/controllers/main.py
@@ -1,11 +1,12 @@
 from odoo import http
 from odoo.addons.base_rest.controllers.main import RestController
+from Flask import send_from_directory
 
 class Academy(http.Controller):
 
     @http.route('/projet/', auth='public')
     def send_report(path):
-        return http.send_static_file("../static/src")
+        return send_from_directory('reports', "../static/src/")
 
     @http.route('/test/', auth='public')
     def send_report(path):
